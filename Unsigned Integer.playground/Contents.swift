2118d2f868060001963f4a5ccc0a98dfc43765c2
Christopher Moore <gkchristopher@icloud.com>
2018-05-09 08:11:02 -0400

    update unsigned int playground

--- Unsigned Integer.playground/Contents.swift
+++ Unsigned Integer.playground/Contents.swift
@@ -2,7 +2,12 @@ import Foundation
 
 var a: UInt8 = 1
 var b: UInt8 = 0xAA
-//var c = 
+var c = UInt8("F6", radix: 16)!
 
-print(String(a, radix: 16))
+a = 0x3
+c = ~a
 
+print(String(a, radix: 2))
+print(String(b, radix: 2))
+print("--------")
+print(String(c, radix: 2))
